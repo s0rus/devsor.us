c90823aaa3c4c2bb907d3a33e48f9df4321474bd
s0rus <piotrmolkontakt@gmail.com>
2023-04-02 16:36:22 +0200

    feat: 🎸 add fonts, scene prototype

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://devsor.us',
-  integrations: [mdx(), sitemap(), tailwind()],
+  output: 'server',
+  integrations: [mdx(), sitemap(), tailwind(), react()],
   server: {
     port: 3001,
   },
---
+++ src/components/Hero.tsx
@@ -0,0 +1,19 @@
+import Spline from '@splinetool/react-spline';
+
+const Hero = () => {
+  return (
+    <div className='w-full h-screen flex justify-center items-center'>
+      <div className='w-full h-full'>
+        <Spline
+          style={{
+            width: '100vw',
+            height: '100vh',
+          }}
+          scene='https://prod.spline.design/c0l34IxRyYOIiU4w/scene.splinecode'
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
