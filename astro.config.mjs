79a63e4d9ecd63ce866f23e8e1ec0bc329554b08
s0rus <piotrmolkontakt@gmail.com>
2023-04-02 21:57:26 +0200

    test: 🧪 deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,9 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-// import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   site: 'https://devsor.us',
-  // output: 'server',
-  // adapter: vercel(),
   integrations: [mdx(), sitemap(), tailwind(), react()],
   server: {
     port: 3001,
--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,16 +1,7 @@
 import Spline from '@splinetool/react-spline';
 
 const Hero = () => {
-  return (
-    <Spline
-      style={{
-        width: '100vw',
-        height: '100vh',
-      }}
-      // scene='https://prod.spline.design/c0l34IxRyYOIiU4w/scene.splinecode'
-      scene='/scene/scene.splinecode'
-    />
-  );
+  return <Spline scene='https://prod.spline.design/c0l34IxRyYOIiU4w/scene.splinecode' />;
 };
 
 export default Hero;
