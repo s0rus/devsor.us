f29a3abd033c0111335037e764c50c4fedf4d851
s0rus <piotrmolkontakt@gmail.com>
2023-04-05 16:47:18 +0200

    test: 🧪 test deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,14 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import image from '@astrojs/image';
 
-import vercel from '@astrojs/vercel/serverless';
+// import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://devsor.us',
   integrations: [mdx(), sitemap(), tailwind(), react(), image()],
   server: {
     port: 3001,
   },
-  output: 'server',
-  adapter: vercel(),
+  // output: 'server',
+  // adapter: vercel(),
 });
