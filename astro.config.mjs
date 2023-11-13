fb0928451279b0b41c0c8984e96532e5db112fc2
soruse <piotrmolkontakt@gmail.com>
2023-11-13 16:14:55 +0100

    test: 🧪 font import

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
@@ -11,4 +12,5 @@ export default defineConfig({
     port: 3001,
   },
   output: "hybrid",
+  adapter: vercel(),
 });
