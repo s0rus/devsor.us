5eff8bd8faec573614d6150af56e62a16adf89b3
soruse <piotrmolkontakt@gmail.com>
2024-04-03 15:12:12 +0200

    feat: 🎸 add projects, up deps

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,7 @@ const projects = defineCollection({
   schema: z.object({
     title: z.string().min(1),
     description: z.string().min(1),
-    repoUrl: z.string().url(),
+    repoUrl: z.string().url().optional(),
     demoUrl: z.string().url().optional(),
     // ? weight determines the order of projects presented on /projects
     // ? -> the bigger the weight the higher on the list item will be
---
+++ src/content/projects/agrogust.md
@@ -0,0 +1,12 @@
+---
+title: "agrogust"
+description: "Landing page for agrogust.pl"
+demoUrl: "https://agrogust.pl/"
+weight: 11
+---
+
+Landing page for [agrogust.pl](https://agrogust.pl/) built with:
+
+- NextJS
+- Tailwind
+- shadcn/ui
